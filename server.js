aa1f6548b75daa6209205ad6e9c5b1d6361aff20
Elyfaistella <75172647+Elyfaistella@users.noreply.github.com>
2020-12-29 22:43:52 +0800

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ var server = http.createServer(function(request, response){
   if(path === '/'){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(`二哈`)
+    response.write(`<h1>恭喜你找到了我</h1>`)
     response.end()
   } else if(path === '/x'){
     response.statusCode = 200
